5b08080d21c81801aa6c22b097e9e25ce0198aae
Abhishek Kumar Ojha <Abhishek.Kumar.Ojha@sumitgupta98374gmail.onmicrosoft.com>
2025-09-23 11:51:32 +0530

    chore(terraform): set subscription to 27ca6581-69fe-4fbb-8791-249910df796d in Mytodo-infra environments

---
+++ environments/dev/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+  
+  # backend "azurerm" {
+  #   resource_group_name  = "backendrg"
+  #   storage_account_name = "backendstorge"
+  #   container_name       = "tfstate"
+  #   key                  = "dev.terraform.tfstate"
+  # }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "27ca6581-69fe-4fbb-8791-249910df796d"
+}
